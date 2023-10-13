20b9c6636ace3d25dc4ed87485c3256a78f733ea
Loic <loicdavykaze@gmail.com>
2023-10-13 12:31:23 -0500

    "first commit"

---
+++ Marvel_Characters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvel_Characters
+//
+//  Created by Loic Kaze on 10/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
